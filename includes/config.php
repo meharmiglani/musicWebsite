08487fdf3d033aafdc175572d0366896f07db9e8
meharmiglani <34315579+meharmiglani@users.noreply.github.com>
2020-08-22 23:09:59 +0530

    Updated config.php

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@
 
 	$timezone = date_default_timezone_set("Europe/London");
 
-	$con = mysqli_connect("localhost", "root", "", "music");
+	$con = mysqli_connect("nwhazdrp7hdpd4a4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "vevmf4s4kj1hboyu", "jfgtp762v8cn489g", "k5wge4zcsyza92fl");
 
 	if(mysqli_connect_errno()){
 		echo "Failed to connect: " . mysqli_connect_errno();
